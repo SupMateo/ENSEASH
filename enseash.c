9d0438b4a57c61441b9ebfff87fb24d27f29a11b
SupMateo <mateo.suprano@ensea.fr>
2023-12-04 08:53:56 +0100

    Question 6

--- enseash.c
+++ enseash.c
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/wait.h>
-#include "enseash.h"
 
 #define BUFSIZE_COMMAND 1024
 #define FD_TERMINAL_OUT 1
@@ -14,72 +13,74 @@
 #define SIGN_INT 1
 #define NO_INT -1
 
+void display_time();
+void display_prompt(int type, int value, long execution_time);
+void execute_command(char *command);
+
 int main(int argc, char **argv)
 {
     char command[BUFSIZE_COMMAND];
-
     int ret, status;
-	pid_t pid;
+    pid_t pid;
     int last_type = NO_INT;
     int last_value = NO_INT;
+    long execution_time;
 
-	long execution_time;
-
-
-	struct timespec start, end;
+    struct timespec start, end;
 
     write(FD_TERMINAL_OUT, "Welcome to ENSEA's Shell.\n", strlen("Welcome to ENSEA's Shell.\n"));
     write(FD_TERMINAL_OUT, "Write 'exit' to stop the program\n", strlen("Write 'exit' to stop the program\n"));
 
     while (1)
     {
-
         display_prompt(last_type, last_value, execution_time);
         ret = read(FD_TERMINAL_IN, command, BUFSIZE_COMMAND - 1);
         command[ret - 1] = 0;
+        
+
         if (strcmp(command, "exit") == 0)
         {
             write(FD_TERMINAL_OUT, "Bye bye :)\n", strlen("Bye bye :)\n"));
             exit(EXIT_SUCCESS);
         }
-		
-        if ((pid = fork()) == -1){
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		clock_gettime(CLOCK_MONOTONIC, &start);
 
-		if (pid != 0)
-		{
-			wait(&status);
-			clock_gettime(CLOCK_MONOTONIC, &end);
-			execution_time = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000;
+        if ((pid = fork()) == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
 
+        clock_gettime(CLOCK_MONOTONIC, &start);
 
-			if (WIFEXITED(status))
-			{
-				last_type = EXIT_INT;
-				last_value = WEXITSTATUS(status);
-			}
-			else if (WIFSIGNALED(status))
-			{
-				last_type = SIGN_INT;
-				last_value = WTERMSIG(status);
-			}
-		}
-		else
-		{
-			if (!command[0])
-			{
-				display_time();
-				exit(EXIT_SUCCESS);
-			}	
-			else if ((execlp(command, (char *)NULL)) == -1)
-			{
-				perror("execlp");
-				exit(EXIT_FAILURE);
-			}
-		}
+        if (pid != 0)
+        {
+            wait(&status);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            execution_time = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000;
+
+            if (WIFEXITED(status))
+            {
+                last_type = EXIT_INT;
+                last_value = WEXITSTATUS(status);
+            }
+            else if (WIFSIGNALED(status))
+            {
+                last_type = SIGN_INT;
+                last_value = WTERMSIG(status);
+            }
+        }
+        else
+        {
+            if (!command[0])
+            {
+                display_time();
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                execute_command(command);
+            }
+        }
     }
 }
 
@@ -96,7 +97,7 @@ void display_time()
 
 void display_prompt(int type, int value, long execution_time)
 {
-	char s[BUFSIZE_COMMAND];
+    char s[BUFSIZE_COMMAND];
     if (type == EXIT_INT)
     {
         sprintf(s, "ENSEASH [exit %d | %dms] %% ", value, execution_time);
@@ -104,11 +105,32 @@ void display_prompt(int type, int value, long execution_time)
     }
     else if (type == SIGN_INT)
     {
-        sprintf(s,"ENSEASH [sign %d | %dms] %% ", value, execution_time);
+        sprintf(s, "ENSEASH [sign %d | %dms] %% ", value, execution_time);
         write(FD_TERMINAL_OUT, s, strlen(s));
     }
     else if (type == NO_INT)
     {
         write(FD_TERMINAL_OUT, "ENSEASH %% ", strlen("ENSEASH %% "));
+    }
+}
+
+void execute_command(char *command)
+{
+    char *arg;
+    char *args[BUFSIZE_COMMAND];
+    int i = 0;
+
+    arg = strtok(command, " ");
+    while (arg != NULL)
+    {
+        args[i++] = arg;
+        arg = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    if (execvp(args[0], args) == -1)
+    {
+        perror("execvp");
+        exit(EXIT_FAILURE);
     }
 }
